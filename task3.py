43b36696e88d11736f8e79131e6f0c60403e53f1
Darya Ryashina <da.ryashina@mail.ru>
2023-12-19 11:29:53 +0300

    Add Python task files

---
+++ task3.py
@@ -0,0 +1,8 @@
+num1 = 7
+num2 = 3
+
+average = (num1 + num2) / 2
+geometric_mean = (num1 * num2) ** 0.5
+
+print("Среднее арифметическое:", average)
+print("Среднее геометрическое:", geometric_mean)
